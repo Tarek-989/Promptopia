48196f8fd881a2932d770334a2c64cf6af1861fc
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-08-01 20:26:30 +0300

    handle search functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,18 +18,54 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 
 const Feed = () => {
+	const [allPosts, setAllPosts] = useState([]);
+
+	// Search states
 	const [searchText, setSearchText] = useState('');
-	const [posts, setPosts] = useState([])
-	const handleSearchChange = (e) => { }
+	const [searchTimeout, setSearchTimeout] = useState(null);
+	const [searchedResults, setSearchedResults] = useState([]);
+
+	const fetchPosts = async () => {
+		const response = await fetch('/api/prompt');
+		const data = await response.json();
+
+		setAllPosts(data);
+	};
 
 	useEffect(() => {
-		const fetchPosts = async () => {
-			const responce = await fetch('/api/prompt');
-			const data = await responce.json();
-			setPosts(data);
-		}
 		fetchPosts();
 	}, []);
+
+	const filterPrompts = (searchtext) => {
+		const regex = new RegExp(searchtext, 'i'); // 'i' flag for case-insensitive search
+		return allPosts.filter(
+			(item) =>
+				regex.test(item.creator.username) ||
+				regex.test(item.tag) ||
+				regex.test(item.prompt)
+		);
+	};
+
+	const handleSearchChange = (e) => {
+		clearTimeout(searchTimeout);
+		setSearchText(e.target.value);
+
+		// debounce method
+		setSearchTimeout(
+			setTimeout(() => {
+				const searchResult = filterPrompts(e.target.value);
+				setSearchedResults(searchResult);
+			}, 500)
+		);
+	};
+
+	const handleTagClick = (tagName) => {
+		setSearchText(tagName);
+
+		const searchResult = filterPrompts(tagName);
+		setSearchedResults(searchResult);
+	};
+
 	return (
 		<section className='feed'>
 			<form className='relative w-full flex-center'>
@@ -42,12 +78,18 @@ const Feed = () => {
 					className='search_input peer'
 				/>
 			</form>
-			<PromptCardList
-				data={posts}
-				handleTagClick={() => { }}
-			/>
-		</section>
-	)
-}
 
-export default Feed
+			{/* All Prompts */}
+			{searchText ? (
+				<PromptCardList
+					data={searchedResults}
+					handleTagClick={handleTagClick}
+				/>
+			) : (
+				<PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+			)}
+		</section>
+	);
+};
+
+export default Feed;
