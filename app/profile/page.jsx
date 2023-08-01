523a8d5ccbb59988ff895a4af3f39a0e5fb41960
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-08-01 20:18:58 +0300

    handle Edit & delete functionality

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,6 +6,7 @@ import Profile from '@components/profile';
 
 const MyProfile = () => {
 	const { data: session } = useSession();
+	const router = useRouter();
 
 	const [posts, setPosts] = useState([])
 	useEffect(() => {
@@ -14,11 +15,28 @@ const MyProfile = () => {
 			const data = await responce.json();
 			setPosts(data);
 		}
-		if(session?.user.id) fetchPosts();
+		if (session?.user.id) fetchPosts();
 	}, []);
-	const handleEdit = () => { };
+	const handleEdit = (post) => {
+		router.push(`/update-prompt?id=${post._id}`);
+	};
 
-	const handleDelete = async () => { };
+	const handleDelete = async (post) => {
+		const hasConfirmed = confirm('Are you sure you want to delete this prompt?');
+		if (hasConfirmed) {
+			try {
+				await fetch(`/api/prompt/${post._id.toString()}`, {
+					method: 'DELETE',
+				});
+
+				const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+				setPosts(filteredPosts);
+			} catch (error) {
+				console.log(error)
+			}
+		}
+	};
 
 	return (
 		<Profile
